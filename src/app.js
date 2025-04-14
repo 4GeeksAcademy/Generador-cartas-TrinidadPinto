07fbdbee23a878ff5789e1127f519762e7dff608
TrinidadPinto <trinidadpintoleon@gmail.com>
2025-04-14 18:29:27 -0400

    generador cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = function () {
+  function generarCarta() {
+    //write your code here
+    const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const valor = valores[Math.floor(Math.random() * valores.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    const pintaCarta = Math.floor(Math.random() * 4) + 1;
+    let pintaSimbolo = "";
+    let clasePalo = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    if (pintaCarta === 1) {
+      pintaSimbolo = "♠";
+      clasePalo = "spade";
+    } else if (pintaCarta === 2) {
+      pintaSimbolo = "♥";
+      clasePalo = "heart";
+    } else if (pintaCarta === 3) {
+      pintaSimbolo = "♦";
+      clasePalo = "diamond";
+    } else {
+      pintaSimbolo = "♣";
+      clasePalo = "club";
+    }
+
+    const topLeft = document.getElementById("top-left");
+    const bottomRight = document.getElementById("bottom-right");
+    const center = document.getElementById("card-value");
+    const carta = document.getElementById("carta");
+
+    topLeft.textContent = pintaSimbolo;
+    bottomRight.textContent = pintaSimbolo;
+    center.textContent = valor;
+
+    carta.className = `card ${clasePalo}`;
+  }
+  generarCarta();
+  const boton = document.getElementById("new-card-btn");
+  boton.addEventListener("click", generarCarta);
 };
